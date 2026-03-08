a1a4a55544100113f1585d0ba5fd2fff647e4fb4
Pros-never-die <asma.khan.game@gmail.com>
2026-03-09 00:47:06 +0530

    Change base URL in Vite config to lowercase

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'Minios1', // Replace with your actual GitHub repo name
+  base: 'minios1', // Replace with your actual GitHub repo name
 });
 
